5825e6e2e49b77b7b43335d2cfc13374c1bb1612
Mauricio Peniche <mauricio.peniche@gmail.com>
2023-08-04 11:28:50 -0600

    feat: new changes in 04/08

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,20 +14,28 @@ function changeColor( elementHtml, color) {
 
 function changeColorWithPrompt( elementHtml) {
     const color = prompt("Dime el color en inglés", "yellow");
-    elementHtml.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+    changeTextOfUserColor( elementHtml, color);
     changeColor( elementHtml, color );
+}
+
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+}
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
 }
 
 function resetColor(){
     const refRedColor = document.getElementById("red-color");
     const refGreenColor = document.getElementById("green-color");
     const refPurpleColor = document.getElementById("purple-color");
-    const refMyColor = document.getElementById("my-color");
+    const refUserColor = document.getElementById("user-color");
 
     /* refRedColor.style.color = "black" */
     changeColor(refRedColor, 'black');
     changeColor(refGreenColor, 'black');
     changeColor(refPurpleColor, 'black');
-    changeColor(refMyColor, 'black');
+    changeColor(refUserColor, 'black');
+    changeTextOfUserColor( refUserColor, 'black');
     
 }
