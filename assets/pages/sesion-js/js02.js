b72c23187d3552a9bd2a212f09595ab443b6337c
Mauricio Peniche <mauricio.peniche@gmail.com>
2023-08-07 16:19:02 -0600

    funciones autoinvocadas

--- assets/pages/sesion-js/js02.js
+++ assets/pages/sesion-js/js02.js
@@ -1,9 +1,21 @@
 console.log("Sesión JS02 Uso de funciones");
 
 /*
+
+https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+
+Función: Conjunto de instrucciones que realiza una tarea o calcula un valor.
+  Código que se puede reutilizar.
+
   ----------- Funciones declaradas --------------
     (function declaration, function statement)
 Una característica de las funciones declradas es que tien hoisting.
+
+sintaxis: 
+ function nombreFuncionCamelCase ( parámetros ){
+        //cuerpo de la función
+        instrucciones;
+    }
 
 hoisting: comportamiento en JS que permite a las declaración de variables(var)
 o funciones se eleven al comienzo de su ámbito antes de que se ejecute el código.
@@ -14,3 +26,48 @@ console.log( multiplica(4, 3));
 function multiplica(a, b){
     return a * b;
 }
+
+/*
+------------------Funciones expresadas ---------------------------------------
+                  (Function expressions)
+
+    Son funciones declaradas dentro de la asignación de una variable. Estas funciones pueden ser anónimas ( no tienen nombre )
+    Las funciones expresadas no teinen hoisting, por que no se carga en memoria hasta que se utilice. 
+
+    sintaxis: 
+          const nombreVariable = function nombreFuncion (parametros){
+            instrucciones;
+          };
+*/
+
+const sum = function sumatoria(a, b){
+  return a + b;
+}
+  
+console.log( `El resultado de 4 + 2 : ${ sum(4,2) }`);
+
+let saludo; // undefined
+const localidad = "Tlaxcala";
+
+if ( localidad == "Buenos Aires"){
+  saludo = function ( name ) { return `Holis ${name}`};
+ }
+else {
+  saludo = function ( name ) { return `Buenos dias ${name} `};
+}
+
+console.log( saludo("Sergio"));
+console.log( saludo("David"));
+
+/*
+---------------- Funciones autoinvocadas --------------
+                (self-invoking functions)
+Funciones que se autoinvocan, se pueden definir con funciones anónimas.
+
+sintaxis: 
+  (function (parámetros ) {} )( argumentos );
+
+*/
+( function( name ){
+    console.log(`Me llamo ${name} y me autoinvoco`);
+} )( "ChatGPT" ); 
