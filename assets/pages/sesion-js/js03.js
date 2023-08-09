81f800c041d8d16155ed365ac03ea9f984a57774
Mauricio Peniche <mauricio.peniche@gmail.com>
2023-08-09 11:05:45 -0600

    ci: declaracion de bloque

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -12,18 +12,38 @@ console.log("No olvidaremos el reto a Ed");
     Si la variable en el bloque tiene el mismo nombre que una variable fuera del bloque se le da prioridad a la variable dentro del bloque,
 
 */
+/* let firstname = "Lalo";
+const lastname = "Garcia"; */
+var age = "24";
 
 let firstname = "Sergio";
 const lastname = "Torres";
 var age = "39";
 
+
 {
     let firstname = "Mau";
     const lastname = "Peniche";
     var age = "26";
+    const colorFavorito = "verde";
+    var comidaFavorita = "pozole";
+
     console.log(`${firstname} ${lastname}`); // Mauricio Peniche
     console.log( age); // 26
+    console.log( colorFavorito); //
+    console.log( comidaFavorita); //
+    {
+        const colorFavorito = "morado";
+        console.log(colorFavorito); // morado
+        console.log( firstname); // mau
+        {
+            console.log(colorFavorito); // morado 
+            console.log( firstname); // mau
+        }
+    }
 }
 
-console.log(`${firstname} ${lastname}`); // Mauricio Peniche
+console.log(`${firstname} ${lastname}`); // Sergio Torres
 console.log( age); // 26
+// console.log( colorFavorito); // colorFavorito no está definido
+console.log( comidaFavorita); // Pozole
